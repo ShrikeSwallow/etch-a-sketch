2293a5450b52c784ab25e2413e5f75baec2b16b0
Maciej Jaskolka <maciej.jaskolka@gmail.com>
2024-10-25 08:17:00 +0100

    Improve the grid generation
    I realised the original implementation was too
    'stiff' and not really implementing flexbox philosophy.
    Changed some semi-fixed calculations to more relative
    ones. Ensured as much flexibility on page resize
    as I could think of.

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const container = document.querySelector(".container");
-console.log("Container assigned.");
+const body = document.body;
+const container = document.createElement("div");
+container.classList.add("container");
 
-const containerWidth = Number.parseInt(
-  window.getComputedStyle(container).width
-);
-
+if (window.innerHeight > window.innerWidth) {
+  container.style.width = "90vw";
+  container.style.height = "90vw";
+} else {
+  container.style.width = "90vh";
+  container.style.height = "90vh";
+}
+body.appendChild(container);
+console.log("Container generated");
 const createGrid = (size) => {
-  console.log(containerWidth);
-
   for (let i = 0; i < Math.pow(size, 2); i++) {
     const pixel = document.createElement("div");
     pixel.classList.add("pixel");
-    const pixelSize = containerWidth / size;
-    pixel.style.minWidth = `${pixelSize}px`;
-    pixel.style.minHeight = `${pixelSize}px`;
+    const pixelSize = 100 / size;
+    pixel.style.minWidth = `${pixelSize}%`;
+    pixel.style.minHeight = `${pixelSize}%`;
     container.appendChild(pixel);
   }
 };
-createGrid(5);
+createGrid(64);
